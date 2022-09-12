be89a797338be5f75ed12d19181a9edaa3c14586
DESKTOP-E1VQ9LR\msi <kmw4907@gmail.com>
2022-09-12 18:04:43 +0900

    1234556

---
+++ src/com/GolForYou/gittest.java
@@ -0,0 +1,13 @@
+package com.GolForYou;
+
+public class gittest {
+	public gittest() {
+		System.out.println("Hello git");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new gittest();
+	}
+
+}
